d9cc0a4e29d6f5b2b2915235c06441dad00d985f
R7558 <rupsanaworld1004@gmail.co>
2023-07-26 02:08:00 +0600

    added update all

--- pages/Slider.js
+++ pages/Slider.js
@@ -5,35 +5,35 @@ const Slider = () => {
            
               <div>
                 {/* <h1 className="lg:ml-10 sm:lm-5  text-xl font-bold text-cyan-950">WELCOME</h1> */}
-                 <div className="ml-10 ">
+                 <div className="ml-10 slider">
           
           <div className=" mt-10  carousel">
-            <div id="slide1" className="relative  w-full carousel-item">
+            <div id="slide1" className="relative  w-full  carousel-item">
             <Image src='/images/flower.jfif' width={950} height={200} alt=""></Image>
               <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide4" className="btn btn-circle">❮</a> 
-                <a href="#slide2" className="btn btn-circle">❯</a>
+                <a href="#slide4" className="btn btn-circle buttn">❮</a> 
+                <a href="#slide2" className="btn btn-circle buttn">❯</a>
               </div>
             </div> 
             <div id="slide2" className="relative w-full carousel-item">
             <Image src='/images/flower.jfif' width={950} height={200} alt=""></Image>
               <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide1" className="btn btn-circle">❮</a> 
-                <a href="#slide3" className="btn btn-circle">❯</a>
+                <a href="#slide1" className="btn btn-circle buttn">❮</a> 
+                <a href="#slide3" className="btn btn-circle buttn">❯</a>
               </div>
             </div> 
             <div id="slide3" className="relative w-full carousel-item">
              <Image src='/images/flower.jfif' width={950} height={200} alt=""></Image>
               <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide2" className="btn btn-circle">❮</a> 
-                <a href="#slide4" className="btn btn-circle">❯</a>
+                <a href="#slide2" className="btn btn-circle buttn">❮</a> 
+                <a href="#slide4" className="btn btn-circle buttn">❯</a>
               </div>
             </div> 
             <div id="slide4" className="relative w-full carousel-item">
             <Image src='/images/flower.jfif' width={950} height={200} alt=""></Image>
               <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide3" className="btn btn-circle">❮</a> 
-                <a href="#slide1" className="btn btn-circle">❯</a>
+                <a href="#slide3" className="btn btn-circle buttn">❮</a> 
+                <a href="#slide1" className="btn btn-circle buttn">❯</a>
               </div>
             </div>
           </div>
