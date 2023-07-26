01a624030be031c4e75c6c8832cf5fb4e817461a
R7558 <rupsanaworld1004@gmail.co>
2023-07-26 21:25:06 +0600

    added update all

--- pages/Authprovider.js
+++ pages/Authprovider.js
@@ -1,19 +1,19 @@
-// "use client"
+"use client"
 
-// import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
-// import auth from '.firebase.init';
-
-
+import auth from '.firebase.init';
 
 
-// const Authprovider = ({children}) => {
-//     const [user, loading] = useAuthState(auth);
 
 
-//     if(!user){
-//         return <Navigate to="/login" state={{ from: location }} replace></Navigate>
-//     }
-//     return children;
-// };
-// export default Authprovider;
+const Authprovider = ({children}) => {
+    const [user, loading] = useAuthState(auth);
+
+
+    if(!user){
+        return <Navigate to="/login" state={{ from: location }} replace></Navigate>
+    }
+    return children;
+};
+export default Authprovider;
